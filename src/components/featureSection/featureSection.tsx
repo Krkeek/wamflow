05642d728686c44ae9b41c3ba079a4847bd868eb
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2024-05-14 16:15:42 +0200

    features implementation

--- src/components/featureSection/featureSection.tsx
+++ src/components/featureSection/featureSection.tsx
@@ -1,11 +1,12 @@
 import styles from './featureSection.module.css'
 import Footer from "@/components/footer/footer";
+import Features from "@/components/featureSection/features/features";
 const FeatureSection = () =>{
     return(
         <>
             <div className={`${styles.Container}`}>
                 <div className={`${styles.FeatureDiv}`}>
-
+                    <Features />
                 </div>
                 <div className={`${styles.FooterDiv}`}>
                     <Footer />
--- src/components/featureSection/features/features.tsx
+++ src/components/featureSection/features/features.tsx
@@ -1,12 +1,26 @@
 
 import styles from './features.module.css'
+import Image from "next/image";
 const Features = () =>{
     return(
         <>
             <div className={`${styles.Container}`}>
-
+                <div className={`${styles.Feature}`}>
+                    <Image src={'/assets/features/feature1.webp'} alt={'feature'} width={60} height={60}/>
+                    <p>Drag, Drop, Design!</p>
+                </div>
+                <div className={`${styles.Feature}`}>
+                    <Image src={'/assets/features/feature2.webp'} alt={'feature'} width={60} height={60}/>
+                    <p>Export and Import  JSON or RDF</p>
+                </div>
+                <div className={`${styles.Feature}`}>
+                    <Image src={'/assets/features/feature3.webp'} alt={'feature'} width={60} height={60}/>
+                    <p>Markdown-Powered Diagram Creation</p>
+                </div>
             </div>
 
         </>
     );
 }
+
+export default Features
