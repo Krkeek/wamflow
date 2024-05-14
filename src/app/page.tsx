efd3973e003fab554bbf8d3ec60f153cfc1fdecf
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2024-05-14 12:11:55 +0200

    adding hero section BG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,28 @@ export default function Home() {
   return (
     <>
         <div className={`${styles.GridContainer}`}>
-            <div className={`${styles.TopbarContainer}`}>
-                <TopBar />
-            </div>
-            <div className={`${styles.ContentContainer}`}>
-                <div className={`${styles.LeftDiv}`}>
-                    <div className={`${styles.Title}`}>
-                        Revolutionize the way you design and implement <span className={`${styles.TitleSpan}`}>web-based applications</span>
+            <div className={`${styles.BgImage}`}>
+                <div className={`${styles.TopbarContainer}`}>
+                    <TopBar/>
+                </div>
+                <div className={`${styles.ContentContainer}`}>
+                    <div className={`${styles.LeftDiv}`}>
+                        <div className={`${styles.Title}`}>
+                            Revolutionize the way you design and implement <span className={`${styles.TitleSpan}`}>web-based applications</span>
+                        </div>
+                        <div className={`${styles.Description}`}>
+                            Graphical notation that provides a UML-like notation that is specifically tailored to the
+                            needs of inter-organizational web-based applications. Empower your team, visualize, design,
+                            and collaborate effortlessly, bridging the gap between concept and implementation.
+                        </div>
                     </div>
-                    <div className={`${styles.Description}`}>
-                        Graphical notation that provides a UML-like notation that is specifically tailored to the needs of inter-organizational web-based applications. Empower your team, visualize, design, and collaborate effortlessly, bridging the gap between concept and implementation.
+                    <div className={`${styles.RightDiv}`}>
+                        {/*<Image src={'/assets/hero.png'} alt={'hero'} width={736} height={1074} />*/}
                     </div>
                 </div>
-                <div className={`${styles.RightDiv}`}>
 
-                </div>
+
             </div>
-
         </div>
 
     </>
