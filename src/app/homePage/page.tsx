b4b2a1f23f8e8b19bcdc9859db46c783e5137922
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2024-05-20 18:12:06 +0200

    shapeDetailContainer implementation + animation

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -1,11 +1,27 @@
+'use client'
 import DiagramHeader from "@/components/homePageComponents/diagramHeader/diagramHeader";
 import styles from './homePage.module.css'
 import OptionsBar from "@/components/homePageComponents/optionsBar/optionsBar";
 import ShapesContainer from "@/components/homePageComponents/shapesContainer/shapesContainer";
+import ShapeDetailContainer from "@/components/homePageComponents/shapeDetailContainer/shapeDetailContainer";
+import {gsap} from "gsap";
+import {useGSAP} from "@gsap/react";
+import {HomePageAnimation} from "@/animation/HomePageAnimation";
 const HomePage = () =>{
+
+    useGSAP(()=>{
+
+        const ctx = gsap.context(()=>{
+            HomePageAnimation()
+        })
+
+        return () => ctx.revert()
+    })
+
+
     return(
         <>
-            <div className={`${styles.Container}`}>
+            <div className={`${styles.Container} ContainerAnimation`}>
               <div className={`${styles.TopBar}`}>
                   <DiagramHeader />
                   <OptionsBar />
@@ -18,7 +34,7 @@ const HomePage = () =>{
 
                     </div>
                     <div className={`${styles.RightSide}`}>
-
+                        <ShapeDetailContainer />
                     </div>
                 </div>
             </div>
---
+++ src/components/homePageComponents/shapeDetailContainer/shapeDetailContainer.tsx
@@ -0,0 +1,79 @@
+'use client'
+import styles from './shapeDetailContainer.module.css'
+import Image from "next/image";
+import {useState} from "react";
+import {retry} from "next/dist/compiled/@next/font/dist/google/retry";
+
+const ShapeDetailContainer = () =>{
+    const [shapeSelected, setShapeSelected] = useState(true);
+    const [showUri, setShowUri] = useState(false);
+    const [inFront, setInFront] = useState(true);
+
+    return(
+        <>
+
+            {
+                shapeSelected ?
+                (
+                    <div className={`${styles.Container}`}>
+                        <div className={`${styles.Top}`}>
+                            <div className={`${styles.LeftSide}`}>Security Realm</div>
+                            <div className={`${styles.RightSide}`}>
+                                <button className={`${styles.TopButtons}`}><Image src={'/assets/trash.webp'}
+                                                                                  alt={'trash'}
+                                                                                  width={25} height={25}/></button>
+                                <button className={`${styles.TopButtons}`}><Image src={'/assets/tick.webp'}
+                                                                                  alt={'trash'}
+                                                                                  width={25} height={25}/></button>
+
+                            </div>
+                        </div>
+                        <input className={`${styles.Input}`} placeholder={'Name'}/>
+                        <div className={`${styles.UriDiv}`}>
+                            <input className={`${styles.Input}`} placeholder={'Uri'}/>
+                            <button className={`${styles.EyeButton}`}><Image onClick={() => setShowUri(!showUri)}
+                                                                             src={!showUri ? '/assets/eyeClosed.webp' : '/assets/eyeOpened.webp'}
+                                                                             alt={'trash'}
+                                                                             width={25} height={25}/></button>
+                        </div>
+                        <div className={`${styles.DimensionsDiv}`}>
+                            <div className={`${styles.InputView}`}>
+                                <p>W</p>
+                                <input type={'text'} className={`${styles.WidthInput}`}/>
+                                <p>H</p>
+                                <input type={'text'} className={`${styles.WidthInput}`}/>
+                            </div>
+                            <p>pixels</p>
+                        </div>
+                        <div className={`${styles.PositionDiv}`}>
+                            <button className={`${styles.PositionButton}`}><Image
+                                onClick={() => setInFront(true)}
+                                src={'/assets/front.webp'}
+                                alt={'front'}
+                                width={21} height={30}/>
+                                <div className={`${styles.Indicator} ${inFront ? styles.IndicatorActive : ' '} `}></div>
+
+                            </button>
+                            <button className={`${styles.PositionButton}`}><Image
+                                src={'/assets/back.webp'}
+                                onClick={() => setInFront(false)}
+                                alt={'back'}
+                                width={21} height={30}/>
+                                <div
+                                    className={`${styles.Indicator}  ${!inFront ? styles.IndicatorActive : ' '} `}></div>
+                            </button>
+                        </div>
+
+                    </div>
+                ):
+                    (
+                        <div className={`${styles.Container} ${styles.EmptyContainer}`}>
+                            No component selected
+                        </div>
+                    )
+            }
+
+        </>
+    );
+}
+export default ShapeDetailContainer
